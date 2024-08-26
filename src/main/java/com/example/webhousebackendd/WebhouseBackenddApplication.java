33172cf8c3316eb2107a44ba29cbf800a140dcc5
khoa1812 <khoatrinh2909@gmail.com>
2024-08-26 10:30:08 +0700

    create

---
+++ src/main/java/com/example/webhousebackendd/WebhouseBackenddApplication.java
@@ -0,0 +1,13 @@
+package com.example.webhousebackendd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhouseBackenddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhouseBackenddApplication.class, args);
+    }
+
+}
